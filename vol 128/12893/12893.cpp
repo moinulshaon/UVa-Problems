529a74b7413287c3469c29df0e33a1da38c28744
moinulshaon <moinul.shaon@gmail.com>
2016-07-23 19:41:50 +0600

    solved 12893

---
+++ vol 128/12893/12893.cpp
@@ -0,0 +1,61 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long LL;
+
+typedef tree<
+    int,
+    null_type,
+    less<int>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>
+ordered_set;
+//find_by_order
+//order_of_key
+
+#define FO(i,a,b) for (int i = (a); i < (b); i++)
+
+#define PB push_back
+#define FRO freopen("in.txt","r",stdin);
+
+#define CLR(arr) memset( (arr),0,sizeof(arr) );
+#define NEG(arr) memset( (arr),-1,sizeof(arr) );
+#define ALL(v) v.begin(),v.end()
+
+#define X first
+#define Y second
+#define MP make_pair
+
+typedef pair<int,int> pint;
+typedef map<int,int> mint;
+
+void show() {cout<<'\n';}
+template<typename T,typename... Args>
+void show(T a, Args... args) { cout<<a<<" "; show(args...); }
+template<typename T>
+void show_c(T& a) { for ( auto &x:a ){ cout<<x<<" ";}cout<<endl;  }
+
+LL calc(LL x){
+    if ( x == 0 )return 0;
+    else {
+        return calc(x>>1) + (x&1);
+    }
+}
+
+int main(){
+
+    int kase;
+    scanf("%d",&kase);
+
+    LL n;
+    while ( kase-- ){
+        scanf("%lld",&n);
+        printf("%lld\n",calc(n));
+    }
+
+    return 0;
+}
